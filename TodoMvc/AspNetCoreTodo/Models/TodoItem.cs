1ceeefe0a7b76a5808619217593befda9bae77e4
Debasish Das <debasish.das64@yahoo.com>
2023-04-26 16:07:39 -0700

    making priority required

--- TodoMvc/AspNetCoreTodo/Models/TodoItem.cs
+++ TodoMvc/AspNetCoreTodo/Models/TodoItem.cs
@@ -27,7 +27,8 @@ namespace AspNetCoreTodo.Models
 		[Range(1, 20000, ErrorMessage = "{0} is out of range")]
         public int? NumberOfDays { get; set; }
 
-        [Range(0, 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required]
+        [Range(1, 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Priority { get; set; }
 
         //Functions
